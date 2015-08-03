f69b610fae8ff5cf88aa88da1239b758aa920e25
hanjia2014 <hanj@datacom.co.nz>
2015-08-03 16:07:25 +1200

    add server side injection

--- src/MySignalR/Controllers/HomeController.cs
+++ src/MySignalR/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.SignalR.Infrastructure;
+using Microsoft.AspNet.SignalR;
+using MySignalR.Hubs;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +13,23 @@ namespace MySignalR
 {
     public class HomeController : Controller
     {
+        private IConnectionManager _connectionManager;
+        private IHubContext _productMessageHub;
+
+        [FromServices]
+        public IConnectionManager ConnectionManager
+        {
+            get
+            {
+                return _connectionManager;
+            }
+            set
+            {
+                _connectionManager = value;
+                _productMessageHub = _connectionManager.GetHubContext<ProductMessageHub>();
+            }
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
